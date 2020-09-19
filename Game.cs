a8bfe4ee005e9287e18b042604629eb81ab9e685
Joke Heyndels <jo_hannah@outlook.com>
2020-09-19 22:08:24 +0200

    End of guidelines

--- Game.cs
+++ Game.cs
@@ -9,26 +9,79 @@ namespace Spaceman
             Console.WriteLine("Oh hello there, I'm the Doctor. " +
                 "You must be the cryptanalyst from UNIT. \n" +
                 "You're just in time, the Cybermen have created a tractor beam to beam up humans to convert,\n" +
-                "and we need to break the code to disable the beam. Can you do it?");
+                "and we need to break the code to disable the beam. Can you do it? Here's the code, each _ is a letter:");
         }
 
         string codeword;
         string currentWord;
         int maxWrongGuesses;
         int currentWrongGuesses;
+        string[] guesses;
         string[] codewords = new string[] { "Torchwood", "Gallifrey", "Dalek", "River", "Cyberman", "TARDIS" };
         Ufo spaceship = new Ufo();
         
-        Game()
+        public Game()
         {
             Random rnd = new Random();
             int wordIndex = rnd.Next(codewords.Length);
-            codeword = codewords[wordIndex];
+            codeword = codewords[wordIndex].ToLower();
             maxWrongGuesses = 5;
             currentWrongGuesses = 0;
             foreach (char c in codeword)
             {
                 currentWord += "_";
+            }
+        }
+
+        public bool DidWin()
+        {
+            return codeword.Equals(currentWord);
+        }
+
+        public bool DidLose()
+        {
+            return currentWrongGuesses >= maxWrongGuesses;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(spaceship.Stringify());
+            Console.WriteLine(currentWord);
+            Console.WriteLine($"You have {maxWrongGuesses - currentWrongGuesses} wrong guesses remaining.");
+        }
+
+        /* Extra challenge: 
+         * The game doesn’t record past guesses and doesn’t check if a user has already guessed a letter. Add that feature.
+         */
+
+        public void Ask()
+        {
+            Console.WriteLine("Guess a letter");
+            string guess = Console.ReadLine().ToLower();
+            if (guess.Length == 1)
+            {
+                Console.WriteLine($"Your guess was {guess}.");
+                if (codeword.Contains(guess))
+                {
+                    Console.WriteLine($"Yay! {guess.ToUpper()} is in the codeword!");
+                    for (int i = 0; i < codeword.Length; i++)
+                    {
+                        if (codeword[i].ToString() == guess)
+                        {
+                            currentWord = currentWord.Remove(i, 1).Insert(i, guess);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Oh no, {guess} is not in the codeword!");
+                    currentWrongGuesses++;
+                    spaceship.AddPart();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please guess one letter at a time!");
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ namespace Spaceman
         static void Main(string[] args)
         {
             Game.Greeting();
+            Game game = new Game();
+            while (game.DidWin() != true && game.DidLose() != true)
+            {
+                game.Display();
+                game.Ask();
+            }
+
+            if (game.DidWin())
+            {
+                Console.WriteLine("Well done, you've beaten the Cybermen, the human is free!");
+            }
+
+            if (game.DidLose())
+            {
+                Console.WriteLine("Oh no, the Cybermen have succeeded in abducting the human! Better luck next time!");
+            }
         }
     }
 }
